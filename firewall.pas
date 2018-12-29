2fc64b974f369675ccde811842599e5d964186d6
erwan2212 <erwan2212@gmail.com>
2018-12-29 23:08:17 +0100

    Add files via upload

---
+++ firewall.pas
@@ -0,0 +1,91 @@
+unit firewall;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils,comobj,NetFwTypeLib_TLB;
+
+
+procedure AddFirewallExceptionNT6(const Caption, AppPath: wideString;
+    const dir:NET_FW_RULE_DIRECTION_=NET_FW_RULE_DIR_IN;
+    const action:NET_FW_ACTION_=NET_FW_ACTION_ALLOW;
+    const protocol:NET_FW_IP_PROTOCOL_=NET_FW_IP_PROTOCOL_ANY;
+    const ports_or_IcmpTypesAndCodes:widestring='');
+procedure DeleteFromWinFirewallNT6(const RuleName: wideString);
+
+const
+  NET_FW_IP_PROTOCOL_ICMPv4 = 1;
+  NET_FW_IP_PROTOCOL_ICMPv6 = 58;
+
+implementation
+
+//we could add ourselve outbound to the firewall...since we have to run as local admin
+//and remove ourselve on exit...
+procedure AddFirewallExceptionNT6(const Caption, AppPath: wideString;
+    const dir:NET_FW_RULE_DIRECTION_=NET_FW_RULE_DIR_IN;
+    const action:NET_FW_ACTION_=NET_FW_ACTION_ALLOW;
+    const protocol:NET_FW_IP_PROTOCOL_=NET_FW_IP_PROTOCOL_ANY;
+    const ports_or_IcmpTypesAndCodes:widestring='');
+var
+  Profile: Integer;
+  Policy2: OleVariant;
+  RObject: OleVariant;
+  NewRule: OleVariant;
+begin
+  //Profile := NET_FW_PROFILE2_PRIVATE OR NET_FW_PROFILE2_PUBLIC or NET_FW_PROFILE2_DOMAIN;
+  if caption='' then exit;
+  Profile :=NET_FW_PROFILE2_ALL;
+  Policy2 := CreateOleObject('HNetCfg.FwPolicy2');
+  RObject := Policy2.Rules;
+  NewRule := CreateOleObject('HNetCfg.FWRule');
+  NewRule.Name        := Caption;
+  NewRule.Description := Caption;
+  if apppath<>'' then NewRule.ApplicationName := AppPath;
+  NewRule.direction:=dir;
+  NewRule.Protocol := protocol;
+  if (protocol=NET_FW_IP_PROTOCOL_TCP) or (protocol=NET_FW_IP_PROTOCOL_UDP) then
+  begin
+  if (ports_or_IcmpTypesAndCodes<>'') and (dir=NET_FW_RULE_DIR_IN) then
+    begin
+    NewRule.localports:=ports_or_IcmpTypesAndCodes;
+    end;
+  if (ports_or_IcmpTypesAndCodes<>'') and (dir=NET_FW_RULE_DIR_OUT) then
+    begin
+    NewRule.RemotePorts:=ports_or_IcmpTypesAndCodes;
+    end;
+  end;
+  if protocol=NET_FW_IP_PROTOCOL_ICMPv4 then NewRule.IcmpTypesAndCodes:=ports_or_IcmpTypesAndCodes;
+  NewRule.Enabled := True;
+  NewRule.Grouping := '';
+  NewRule.Profiles := Profile;
+  NewRule.Action := action;
+  RObject.Add(NewRule);
+end;
+
+{
+//appname pour nt6 - filename pour nt5
+CoInitialize(nil);
+DeleteFromWinFirewall3(app);
+CoUninitialize;
+}
+
+procedure DeleteFromWinFirewallNT6(const RuleName: wideString);
+var
+  Profile: Integer;
+  Policy2: OleVariant;
+  RObject: OleVariant;
+  policy3: INetFwPolicy2;
+begin
+  //Profile := NET_FW_PROFILE2_PRIVATE OR NET_FW_PROFILE2_PUBLIC or NET_FW_PROFILE2_DOMAIN;
+  Profile :=NET_FW_PROFILE2_ALL;
+  //policy3 := INetFwPolicy2(CreateOleObject( 'HNetCfg.FwPolicy2' ));
+  //policy3.Rules.Remove(rulename);
+  Policy2 := CreateOleObject('HNetCfg.FwPolicy2');
+  RObject := Policy2.Rules;
+  RObject.Remove(RuleName);
+end;
+
+end.
+
--- snif.dpr
+++ snif.dpr
@@ -12,6 +12,7 @@ uses
   windows,
   sysutils,
   winsock,
+  firewall,
   Tsniffer in 'Tsniffer.pas',
   ipheader in 'ipheader.pas',
   pcaptools in 'pcaptools.pas';
@@ -27,9 +28,10 @@ end;
 
 
 //function GetBestInterface(dwDestAddr: Cardinal; pdwBestIfIndex: PDWORD): DWORD; stdcall;external 'iphlpapi.dll';
+//function SendARP (const DestIP, SrcIP: Cardinal; pMacAddr: pointer;var PhyAddrLen: ULONG): DWORD; stdcall;external 'iphlpapi.dll';
 
 var
-cpt:integer;
+cpt:cardinal;
 Msg: TMSG;
 raw_sniffer:TRawSniffer ;
 obj:tobj;
@@ -38,11 +40,15 @@ interfaces:tstrings;
 fromf:file;
 cap:boolean=false;
 error:boolean=false;
+mac:array[0..5] of byte;
+SendARP: function (const DestIP, SrcIP: dword; pMacAddr: pointer;PhyAddrLen: PULONG): DWORD; stdcall=nil;
+
+
 
 procedure open_cap;
 const DLT_EN10MB      =1;
 begin
-AssignFile(FromF, 'dump'+formatdatetime('hh-nn-ss-', now)+'.cap');
+AssignFile(FromF, 'dump'+formatdatetime('hh-nn-ss-zzz', now)+'.cap');
 Rewrite (FromF,1);
 write_cap_header(fromf,DLT_EN10MB);
 end;
@@ -63,9 +69,12 @@ begin
        len:=len+14;
        buf:=allocmem(len); //allocmem=getmem+Initialize
        //ethernet header 14 bytes
-       //we could fill in the mac addresses by resolving ip to mac...
        buf[12]:=#8;buf[13]:=#0;
-       //ip header len
+       //we could fill in the mac addresses by resolving ip to mac...
+       {
+       if PIP_Header(data)^.ip_srcaddr=inet_Addr(PChar(ip)) then copymemory(@buf[6],@mac[0],6);
+       if PIP_Header(data)^.ip_destaddr=inet_Addr(PChar(ip)) then copymemory(@buf[0],@mac[0],6);
+       }
        copymemory(@buf[14],data,len-14);
        write_cap_packet(fromf,len,ptime,buf);
        FreeMem(buf,len);
@@ -148,6 +157,7 @@ if (filter_proto <>'') and (lowercase(str_prot)=lowercase(filter_proto)) then
    if (filter_port =inttostr(src_port)) or (filter_port =inttostr(dest_port))
       then writeln(str_time+' '+str_prot+' '+str_srcip+':'+inttostr(src_port)+' '+str_destip+':'+inttostr(dest_port)+' '+str_len + ' Bytes');
    end;
+
 if filter_proto =''
   then writeln(str_time+' '+str_prot+' '+str_srcip+':'+inttostr(src_port)+' '+str_destip+':'+inttostr(dest_port)+' '+str_len + ' Bytes');
 
@@ -157,16 +167,16 @@ End;
 
 
 begin
-writeln('raw sniffer 0.1 by erwan2212@gmail.com');
-writeln('snif [localip] [proto] [port] [0:1]');
+writeln('raw sniffer 0.2 by erwan2212@gmail.com');
+writeln('snif [localip:x.x.x.x] [proto:udp|tcp|icmp] [port:1-65535] [catpure:0|1] [firewall:0|1]');
 writeln('use * if you want to skip/ignore a parameter');
 writeln('snif 127.0.0.1 tcp 80 1 will filter tcp:80 traffic on localhost and dump all traffic to dump.cap file');
 writeln('snif 127.0.0.1 udp * 0 will filter udp traffic on localhost but not to file');
+writeln('snif 192.168.1.144 * * 0 1 will dump all traffic to console and create a firewall exception to capture incoming traffic');
 
 //try GetBestInterface(0,@dwindex);except end;
 
-if (paramcount>=1) and (paramstr(1)<>'*')
-  then ip:=paramstr(1)
+if (paramcount>=1) and (paramstr(1)<>'*') then ip:=paramstr(1)
   else
   begin
   //ip:=getlocalip;
@@ -179,11 +189,31 @@ if (paramcount>=1) and (paramstr(1)<>'*')
   try ip:=interfaces[cpt];except exit; end;
   end;
 
-raw_sniffer:=TRawSniffer.Create ;
-raw_sniffer.promisc :=true;    
+//if (length(paramstr(1))=1) and (paramstr(1)<>'*') then ip:=TRawSniffer.EnumInterfaces[strtoint(paramstr(1))];
+
+{
+cpt:=6;
+fillchar(mac,6,0);
+@sendarp:=GetProcAddress(LoadLibraryA(PAnsiChar('iphlpapi.dll')),'SendARP'); //case sensitive !!!!
+if @sendarp<>nil then SendARP (inet_Addr(PChar(ip)),0,@mac[0],@cpt) ;
+}
 
 if (paramcount>=2) and (paramstr(2)<>'*') then filter_proto:=paramstr(2);
 if (paramcount>=3) and (paramstr(3)<>'*') then filter_port:=paramstr(3);
+if (paramcount>=4) and (paramstr(4)='1') then cap:=true else cap:=false;
+
+try
+  if (paramcount>=5) and (paramstr(5)='1') then
+  begin
+  writeln('adding inbound firewall exception');
+  AddFirewallExceptionNT6('snif',paramstr(0));
+  end;
+except
+on e:exception do writeln('AddFirewallExceptionNT6 failed:'+e.message);
+end;
+
+raw_sniffer:=TRawSniffer.Create ;
+raw_sniffer.promisc :=true;    
 raw_sniffer.str_ip :=ip;
 raw_sniffer.OnPacket :=obj.OnPacket ;
 raw_sniffer.OnError :=obj.OnError ;
@@ -192,9 +222,10 @@ try
 except
 on e:exception do begin writeln(e.message);exit;end;
 end;
-writeln('sniffing on '+ip);
-if (paramcount>=4) and (lowercase(paramstr(4))='1') then cap:=true;
+
 if cap=true then open_cap ;
+
+writeln('sniffing on '+ip);
 while (GetMessage(Msg,0,0,0))   do
 begin
 {these will get the messages to the window they should go to}
@@ -203,6 +234,17 @@ begin
   DispatchMessage(Msg);
   if HiWord(GetAsyncKeyState(VK_ESCAPE)) <> 0 then break;
 end;
+
 if cap=true then close_cap ;
 raw_sniffer.closesocket_ ;
+
+try
+  if (paramcount>=5) and (paramstr(5)='1') then
+     begin
+     writeln('removing inbound firewall exception');
+     DeleteFromWinFirewallNT6('snif');
+     end;
+except
+on e:exception do writeln('DeleteFromWinFirewall3 failed:'+e.message);
+end;
 end.
